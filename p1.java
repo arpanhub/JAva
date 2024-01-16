0a7a70c7e67a4329bf84e59f8738e7413e86495d
Krishna Kumar <98034368+Krishnaqwerty@users.noreply.github.com>
2024-01-16 19:53:53 +0530

    Update p1.java
    @java: hi...

--- p1.java
+++ p1.java
@@ -1,5 +1,10 @@
 public class p1 {
     public static void main(String[] args) {
         System.out.println("hello guys how are you");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("Comments: ");
+        System.out.println(" ");
+        System.out.println("@Java: @arpanhub Wait a Month bro...");
     }
 }
